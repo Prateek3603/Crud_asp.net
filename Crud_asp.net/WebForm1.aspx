2be756d790f8b50110c72fdad49e4b4769a3155e
Prateek <Kpateek.me@gmail.com>
2023-07-31 04:25:47 +0530

    First commit

---
+++ Crud_asp.net/WebForm1.aspx
@@ -0,0 +1,89 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Crud_asp.net.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>Name:</td>
+                    <td><asp:TextBox ID="txtname" runat="server"></asp:TextBox></td>
+                </tr>  <tr>
+                    <td>Age:</td>
+                    <td><asp:TextBox ID="textAge" runat="server"></asp:TextBox></td>
+                </tr>
+                  <tr>
+                    <td>Gender:</td>
+                    <td><asp:RadioButtonList ID="rbl" runat="server" RepeatColumns="3">
+                        <asp:ListItem Text="Male" Selected="True"  Value="1"></asp:ListItem>
+                         <asp:ListItem Text="FeMale" Selected="True"  Value="2"></asp:ListItem>
+                         <asp:ListItem Text="other" Selected="True"  Value="3"></asp:ListItem>
+                        </asp:RadioButtonList></td>
+                </tr>
+                  <tr>
+                    <td>Address:</td>
+                    <td><asp:TextBox ID="textAddress" runat="server"></asp:TextBox></td>
+
+                </tr>
+                 <tr>
+                    <td></td>
+                    <td><asp:Button ID="btnsave" Text="save" runat="server" OnClick="btnsave_Click"></asp:Button></td>
+
+                </tr>
+                  <tr>
+                    <td></td>
+                    <td><asp:GridView ID="grdview" runat="server" OnRowCommand="grd_RowCommand" AutoGenerateColumns="false">
+                     <Columns>
+                         <asp:TemplateField HeaderText="Name">
+                             <ItemTemplate>
+                                 <%#Eval("Name") %>
+                             </ItemTemplate>
+                         </asp:TemplateField>
+                     
+                          
+                         <asp:TemplateField HeaderText="Age">
+                             <ItemTemplate>
+                                 <%#Eval("Age") %>
+                             </ItemTemplate>
+                         </asp:TemplateField>
+                     
+                          
+                         <asp:TemplateField HeaderText="Gender">
+                             <ItemTemplate>
+                                 <%#Eval("Gender") %>
+                             </ItemTemplate>
+                         </asp:TemplateField>
+                     
+                         
+                         <asp:TemplateField HeaderText="Address">
+                             <ItemTemplate>
+                                 <%#Eval("Address") %>
+                             </ItemTemplate>
+                         </asp:TemplateField>
+                 
+                         <asp:TemplateField HeaderText="Action">
+                             <ItemTemplate>
+                               <asp:LinkButton ID="btndelete" runat="server" Text="Delete" CommandName="AA" CommandArgument='<%#Eval("id") %>'></asp:LinkButton>
+                             </ItemTemplate>
+                         </asp:TemplateField>
+                         <asp:TemplateField HeaderText="Action">
+                             <ItemTemplate>
+                               <asp:LinkButton ID="btnedit" runat="server" Text="Edit" CommandName="BB" CommandArgument='<%#Eval("id") %>'></asp:LinkButton>
+                             </ItemTemplate>
+                         </asp:TemplateField>
+                            
+                     </Columns>
+
+                        </asp:GridView></td>
+
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Crud_asp.net/WebForm1.aspx.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Crud_asp.net
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-621JH0K;Initial Catalog=db_Employee;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                databind();
+                clear();
+            }
+
+
+        }
+
+        public void clear()
+        {
+            txtname.Text = "";
+            textAge.Text="";
+            textAddress.Text = "";
+            rbl.Text = "";
+
+        }
+
+        public void databind()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("usp_get", con);
+            cmd.CommandType = new CommandType();
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            con.Close();
+            grdview.DataSource = dt;
+            grdview.DataBind();
+        }
+        protected void btnsave_Click(object sender, EventArgs e)
+        {
+            if (btnsave.Text == "save")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("usp_insert", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Name", txtname.Text);
+                cmd.Parameters.AddWithValue("@Age", textAge.Text);
+                cmd.Parameters.AddWithValue("@Gender", rbl.SelectedItem.Text);
+                cmd.Parameters.AddWithValue("@Address", textAddress.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                databind();
+                clear();
+            }
+            else if(btnsave.Text=="Update")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("usp_update", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", ViewState["m"]);
+                cmd.Parameters.AddWithValue("@Name", txtname.Text);
+                cmd.Parameters.AddWithValue("@Age", textAge.Text);
+                cmd.Parameters.AddWithValue("@Gender", rbl.SelectedItem.Text);
+                cmd.Parameters.AddWithValue("@Address", textAddress.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                databind();
+                clear();
+            }
+        }
+
+        protected void grd_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "AA")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("usp_delete", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", e.CommandArgument);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                databind();
+            }
+
+            else if(e.CommandName=="BB")
+            {
+
+                con.Open();
+                SqlCommand cmd = new SqlCommand("usp_edit", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", e.CommandArgument);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                con.Close();
+                txtname.Text = dt.Rows[0]["Name"].ToString();
+                textAge.Text = dt.Rows[0]["Age"].ToString();
+                rbl.Text = dt.Rows[0]["Gender"].ToString();
+                textAddress.Text = dt.Rows[0]["Address"].ToString();
+                btnsave.Text = "Update";
+                ViewState["m"] = e.CommandArgument;
+
+
+
+            }
+        }
+        
+        
+    }
+}
